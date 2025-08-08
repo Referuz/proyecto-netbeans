1a4d930b495a17c73a4d5c20ab294a9213cfd3f5
juand <juand@DESKTOP-G6NS2NC>
2025-08-07 18:15:11 -0600

    Primera modificacion para comprovar conexion

--- src/grupos/Grupos.java
+++ src/grupos/Grupos.java
@@ -9,7 +9,7 @@ package grupos;
 public class Grupos {
 
     /*
-     *  Prueva a01 para verificar la conexion. 
+     *  Prueva a02 para verificar la conexion. 
      */
     public static void main(String[] args) {
         // TODO code application logic here
