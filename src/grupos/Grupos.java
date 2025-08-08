4b82729d976bd513ffcb5a552328af5beee4649e
juand <juand@DESKTOP-G6NS2NC>
2025-08-07 18:09:14 -0600

    Creacion del proyecto para administrar lo promedios de un grupo

---
+++ src/grupos/Grupos.java
@@ -0,0 +1,18 @@
+
+package grupos;
+
+/**
+ *
+ * @author juand
+ * 
+ */
+public class Grupos {
+
+    /*
+     *  Prueva a01 para verificar la conexion. 
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
